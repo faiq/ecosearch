36a9718c8760d7c956db30541e078602fdca44cd
faiqus <faiqrazarizvi@gmail.com>
2014-09-10 23:55:09 -0400

    barebones backend for ecosystem search

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express')
+  , logger = require('morgan')
+  , bodyParser = require('body-parser') 
+  , path = require('path')
+  , http = require('http')
+  , showIndex = require('./controllers/showIndex') 
+  //, makeSearch = require('./controllers/makeSearch') 
+  , app = express()
+  , server = http.createServer(app)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(logger('dev'))
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.get('/', showIndex)   
+app.post('/searchEco', makeSearch)
+
+server.listen(3000, function(){
+  console.log('listening on *:3000');
+});
